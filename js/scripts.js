685190a2a179f13fc7dde2daf7d89d636b00b73e
Munene84 <edwinmunene95@gmail.com>
2020-02-15 16:44:06 +0300

    Changed some items on calculator.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,60 +1,41 @@
-function getDayOfWeek() {
+var DayOfWeek=prompt("would you like to get your akan name");
+function DayOfWeek() {
 
-    var year = parseInt(document.getElementById("year").value);
-    var month = parseInt(document.getElementById("month").value);
-    var date = parseInt(document.getElementById("date").value);
-    var male1 = document.getElementById("male1");
-    var female1 = document.getElementById("female1");
-    var yearString = year.toString();
-    var centuryString = yearString.slice(0, 2);
-    var century = parseInt(centuryString);
-    var dayOfWeek = Math.trunc((((century / 4) - 2 * century - 1) + ((5 * year / 4)) + ((26 * (month + 1) / 10)) + date) % 7);
-    var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "yaw", "Kofi", "Kwame"];
-    var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+}
 
-    if (male1.checked == true) {
+    if (Male.checked == true) {
 
-
-    if (year == "") {
-        alert("Please enter the date");
-    } else if (month == "") {
-        alert("please enter the month");
-    } else if (date == "") {
-        alert("please enter the year");
-    } else if (male1.checked == true) {
-        switch (dayOfWeek) {
-            case 0:
-                alert("Your akan name is: " + maleNames[dayOfWeek]);
-                alert("Your akan  is: " + maleNames[dayOfWeek]);
-            case 1:
-                alert("your akan name is: " + maleNames[dayOfWeek]);
-            case 2:
-                alert("Your akan name is: " + maleNames[dayOfWeek]);
-            case 3:
-                alert("Your akan name is: " + maleNames[dayOfWeek]);
-            case 4:
-                alert("Your akan name is: " + maleNmaes[dayOfWeek]);
-            case 5:
-                alert("Your akan name is: " + maleNmaes[dayOfWeek]);
-            case 6:
-                alert("Your akan name is: " + maleNmaes[dayOfWeek]);
-        }
-    }
-    else if (female1.checked == true) {
-        switch (dayOfWeek) {
-            case 0:
-                alert("your akan name is: " + femaleNames[dayOfWeek]);
-                alert("your akan namename is: " + femaleNames[dayOfWeek]);
-            case 1:
-                alert("your akan name is: " + femaleNames[dayOfWeek]);
-            case 2:
-                alert("your akan name is: " + femaleNames[dayOfWeek]);
-            case 3:
-                alert("your akan name is: " + femaleNames[dayOfWeek]);
-            case 4:
-                alert("your akan name is: " + femaleNames[dayOfWeek]);
-                case 5:
-                    alert("your akan name is: " + femaleNames[dayOfWeek]);
-                case 6:
-                    alert("your akan name is: " + femaleNames[dayOfWeek]);
-        }
+switch (dayOfWeek) {
+case 0:
+        alert("Kwasi");
+case 1:
+        alert("Kwadwo");
+case 2:
+        alert("Kwabena");
+case 3:
+        alert("Kwaku");
+case 4:
+       alert("yaw");
+case 5:
+       alert("Kofi");
+case 6:
+        alert("Kwame");
+      }
+    } else if (female.checked == true) {
+switch (dayOfWeek) {
+      case 0:
+              alert("Akosua");
+      case 1:
+              alert("Adwoa");
+      case 2:
+              alert("Abenaa");
+      case 3:
+              alert("Akua");
+      case 4:
+             alert("Yaa");
+      case 5:
+             alert("Afua");
+      case 6:
+              alert("Ama");
+            }
+            }
